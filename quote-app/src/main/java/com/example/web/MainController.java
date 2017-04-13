a3c4646ede3f9acf4edef5f79ee53b0a547bc144
Stephane Nicoll <snicoll@pivotal.io>
2017-04-13 15:28:51 +0200

    Stream demonstration
    This commit uses the new `WebClient` to contact another service
    providing quote values. It then exposes that data as an event stream
    that the client uses to draw a graph dynamically.

--- quote-app/src/main/java/com/example/web/MainController.java
+++ quote-app/src/main/java/com/example/web/MainController.java
@@ -1,10 +1,14 @@
 package com.example.web;
 
 import com.example.domain.UserRepository;
+import reactor.core.publisher.Flux;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Controller
 public class MainController {
@@ -24,6 +28,19 @@ public class MainController {
 	@GetMapping("/quotes")
 	public String quotes() {
 		return "quotes";
+	}
+
+	@GetMapping(path = "/quotes/feed", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	@ResponseBody
+	public Flux<Quote> fetchQuotesStream() {
+		return WebClient.create("http://localhost:8081")
+				.get()
+				.uri("/quotes")
+				.accept(MediaType.APPLICATION_STREAM_JSON)
+				.retrieve()
+				.bodyToFlux(Quote.class)
+				.share()
+				.log();
 	}
 
 }
